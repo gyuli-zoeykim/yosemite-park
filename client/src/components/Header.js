3f09caa9729586e3eee3675804e95e4fc30a44b9
Zoey Kim <gyulikimwork@gmail.com>
2023-08-24 23:43:03 +0000

    Areas of Focus css-styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,41 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MenuItems from './MenuItems';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import logo_icon from '../images/logo_icon.png';
-import logo_text from '../images/logo_text.png';
+import logo_iconGreen from '../images/logo_iconGreen.png';
 import { IoCloseSharp } from 'react-icons/io5';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [prevScroll, setPrevScroll] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const [logoSrc, setLogoSrc] = useState(logo_icon);
+  const [bgColor, setBgColor] = useState(false);
 
   const handleToggle = () => {
     setOpen(!open);
   };
 
+  const handleLogoHover = () => {
+    if (prevScroll !== 0) {
+      setVisible(true);
+      setBgColor(true);
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const menuText = document.querySelector('.menu-text');
+
+      if (currentScrollPos > prevScroll) {
+        setVisible(false);
+        setLogoSrc(logo_iconGreen);
+        setBgColor(false);
+        menuText.classList.add('color');
+      } else {
+        setVisible(true);
+        setLogoSrc(currentScrollPos === 0 ? logo_icon : logo_iconGreen);
+        setBgColor(currentScrollPos === 0 ? false : true);
+        currentScrollPos === 0
+          ? menuText.classList.remove('color')
+          : menuText.classList.add('color');
+      }
+
+      setPrevScroll(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScroll]);
+
   return (
     <div className="header">
-      <div className="header-inner">
-        <div className="logo">
+      <div className="logo-icon-wrapper">
+        <Link className="title" to="/">
+          <img
+            className="logo-icon"
+            src={logoSrc}
+            alt="Logo"
+            onMouseEnter={handleLogoHover}
+          />
+        </Link>
+      </div>
+      <div className="menu-text-wrapper" onClick={handleToggle}>
+        <h4 className={`menu-text ${bgColor && 'color'}`}>Menu</h4>
+      </div>
+      <div className={`bg-color ${bgColor ? 'visible' : 'hidden'}`}></div>
+      <div className={`header-inner ${visible ? 'visible' : 'hidden'}`}>
+        <div className="logo-text-wrapper">
           <Link className="title" to="/">
-            <img className="logo-icon" src={logo_icon} alt="Logo" />
-            <img className="logo-text" src={logo_text} alt="Logo" />
+            <span className={`logo-text ${bgColor && 'color'}`}>YOSEMITE</span>
           </Link>
         </div>
         <div className="menu">
-          <ul className="menu-horizontal">
-            <MenuItems active="horizontal" />
+          <ul className={`menu-horizontal`}>
+            <MenuItems active={`horizontal ${bgColor && 'color'}`} />
           </ul>
-          <div className="menu-icon" onClick={handleToggle}>
-            <h4>Menu</h4>
-          </div>
           {open ? (
-            <div className="icon-panel">
-              <Link className="title" to="/">
-                <img className="logo-icon" src={logo_icon} alt="Logo" />
-              </Link>
-              <IoCloseSharp className="close-icon" onClick={handleToggle} />
-            </div>
+            <IoCloseSharp className="close-icon" onClick={handleToggle} />
           ) : null}
           <ul
             className={`menu-vertical ${open ? 'open' : ''}`}
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
+  const [customScale, setCustomScale] = useState(0);
+  const [columnTwoY, setColumnTwoY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scaledProgress = scrollY / (documentHeight - windowHeight);
+      setCustomScale(scaledProgress);
+      const newY = scaledProgress * 82;
+      setColumnTwoY(newY);
+
+      console.log(scrollY, windowHeight);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <section className="page-home">
       <div className="intro-home">
         <h2 className="intro-head">EXPLORE AND CONSERVE THE PARK YOU LOVE</h2>
         <p className="intro-text">YOSEMITE</p>
       </div>
-      <div className="who-we-are container">
+      <div className="who-we-are section-one container">
         <div className="who-we-are column-one">
           <h2 className="section-title">Who We Are</h2>
           <figure className="who-we-are image">
@@ -20,18 +42,158 @@ const HomePage = () => {
             />
           </figure>
         </div>
-        <div className="who-we-are column-two ratio-two">
+        <div
+          className="who-we-are column-two"
+          style={{
+            transition: 'transform 0.1 ease-out',
+            transform: `translateY(${columnTwoY}px)`,
+          }}>
           Yosemite Conservancy is a nonprofit dedicated to preserving Yosemite
           National Park's resources and providing enriching visitor experiences
           in the park. Our philanthropic partnership with Yosemite goes back 100
           years.
         </div>
-        <div className="who-we-are column-three ratio-one">
+        <div
+          className="sphere"
+          style={{
+            transition: 'transform 0.1s ease-out',
+            transform: `translate(${(0.5 - customScale) * 100}%, ${
+              (customScale - 0.5) * 100
+            }%) scale(${customScale})`,
+          }}></div>
+        <div className="who-we-are column-three">
           <div className="line"></div>
           <Link className="who-we-are title" to="/">
             <span>Learn More</span>
             <span style={{ marginLeft: '5px' }}>➔</span>
           </Link>
+        </div>
+      </div>
+      <div className="what-we-do section-two container">
+        <div className="what-we-do row-one">
+          <h3 className="section-title">What We Do</h3>
+        </div>
+        <div className="what-we-do row-two">
+          <div className="what-we-do detail">
+            <figure className="what-we-do image">
+              <img
+                className="responsive-image"
+                src="https://images.unsplash.com/photo-1441239372925-ac0b51c4c250?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80"
+                alt="yosemite"
+              />
+            </figure>
+            <div>
+              <h5 className="detail-title">Yosemite Falls</h5>
+            </div>
+            <div>
+              <p className="detail-text">
+                A much-needed project revamped the approach to Lower Yosemite
+                Fall, one of the Valley’s most visited destinations,
+                dramatically improving the visitor experience and protecting the
+                surrounding ecosystem.
+              </p>
+            </div>
+          </div>
+          <div className="what-we-do detail">
+            <figure className="what-we-do image">
+              <img
+                className="responsive-image"
+                src="https://images.unsplash.com/photo-1602743118789-9bb32eda5047?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+                alt="yosemite"
+              />
+            </figure>
+            <div>
+              <h5 className="detail-title">Scenic Overlooks</h5>
+            </div>
+            <div>
+              <p className="detail-text">
+                A series of donor-funded restoration projects transformed four
+                classic Yosemite overlooks: Glacier Point, Tunnel View, Olmsted
+                Point and Half Dome View.
+              </p>
+            </div>
+          </div>
+          <div className="what-we-do detail">
+            <figure className="what-we-do image">
+              <img
+                className="responsive-image"
+                src="https://images.unsplash.com/photo-1625442827958-547189bc4e55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1664&q=80"
+                alt="yosemite"
+              />
+            </figure>
+            <div>
+              <h5 className="detail-title">Keeping Bears Wild</h5>
+            </div>
+            <div>
+              <p className="detail-text">
+                Upgrade successful bear monitoring and tracking technology,
+                including GPS collars, and continue educating the public through
+                the ongoing maintenance of KeepBearsWild.org.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="what-we-do row-three">
+          <Link className="what-we-do title" to="/">
+            <span>Learn More</span>
+            <span style={{ marginLeft: '5px' }}>➔</span>
+          </Link>
+        </div>
+      </div>
+      <div className="areas-of-focus section-three container">
+        <div className="areas-of-focus description">
+          <div className="areas-of-focus">
+            <h3 className="section-title">Areas of Focus</h3>
+          </div>
+          <p className="description-text">
+            Every year, we fund critical work in Yosemite National Park. That
+            work covers a variety of topics, from trail repairs to wildlife
+            management to youth programs.
+          </p>
+        </div>
+        <div className="areas-of-focus icons">
+          <div className="items">
+            <div className="contents">
+              <figure className="areas-of-focus logo">
+                <img
+                  className="responsive-image"
+                  src="https://images.unsplash.com/photo-1625442827958-547189bc4e55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1664&q=80"
+                  alt="yosemite"
+                />
+              </figure>
+              <h6>WILDLIFE MANAGEMENT</h6>
+            </div>
+            <div className="contents">
+              <figure className="areas-of-focus logo">
+                <img
+                  className="responsive-image"
+                  src="https://images.unsplash.com/photo-1625442827958-547189bc4e55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1664&q=80"
+                  alt="yosemite"
+                />
+              </figure>
+              <h6>WILDLIFE MANAGEMENT</h6>
+            </div>
+            <div className="contents">
+              <figure className="areas-of-focus logo">
+                <img
+                  className="responsive-image"
+                  src="https://images.unsplash.com/photo-1625442827958-547189bc4e55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1664&q=80"
+                  alt="yosemite"
+                />
+              </figure>
+              <h6>WILDLIFE MANAGEMENT</h6>
+            </div>
+            <div className="contents">
+              <figure className="areas-of-focus logo">
+                <img
+                  className="responsive-image"
+                  src="https://images.unsplash.com/photo-1625442827958-547189bc4e55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1664&q=80"
+                  alt="yosemite"
+                />
+              </figure>
+              <h6>WILDLIFE MANAGEMENT</h6>
+            </div>
+          </div>
         </div>
       </div>
     </section>
